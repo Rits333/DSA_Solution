2dc363bcd5591efb3d69bfa675bae04ba2705ef1
My name <me@example.com>
2021-06-21 13:52:28 +0530

    DSA question

---
+++ Recurssion/Day1/Day1_Q1_.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def F(n) :
+    if n == 2:
+        return 1
+    elif n == 1:
+        return 0
+    else:
+        return F(n-1)+F(n-2)
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
